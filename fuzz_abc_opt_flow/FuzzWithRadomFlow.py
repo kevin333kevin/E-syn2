6ab29e70ca93d74137889c981aa6523cedd19cf8
GaryHU <swe1609011@gmail.com>
2024-02-25 19:11:32 +0800

    update Fuzzing OPT flow, exploring the ABC optimization space

---
+++ fuzz_abc_opt_flow/FuzzWithRadomFlow.py
@@ -0,0 +1,36 @@
+import os
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+# Function to execute a single ABC flow
+def execute_flow(flow):
+    # Prepare the command with the given flow
+    command = "../abc/abc -c \"abcrc; read_eqn ../abc/ori.eqn; strash; {} ;read_lib ../abc/asap7_clean.lib ; map ; topo; upsize; dnsize; stime\"".format(flow)
+    # Execute the command
+    os.system(command)
+    
+    return "Completed: {}".format(flow)
+
+def main():
+    # Load the flows from file
+    with open('res.txt', 'r') as file:
+        flows = [line.strip() for line in file]
+    
+    # Number of parallel processes (up to the number of cores/cpus you have)
+    max_workers = 64
+    
+    # Create a process pool and execute the flows in parallel
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+        # Submit all the flows for execution
+        future_to_flow = {executor.submit(execute_flow, flow): flow for flow in flows}
+        
+        # Process the results as they complete
+        for future in as_completed(future_to_flow):
+            flow = future_to_flow[future]
+            try:
+                result = future.result()
+                print(result)  # Or handle the result in another way
+            except Exception as exc:
+                print('%r generated an exception: %s' % (flow, exc))
+
+if __name__ == '__main__':
+    main()
---
+++ fuzz_abc_opt_flow/RandomFlowGenerator.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Commands array
+cmds=("rw" "rwz" "rf" "rfz" "balance" "resub" "resub -z")
+
+# Function to generate a random sequence
+generate_sequence() {
+    local len=$1  # Length of the sequence
+    local seq=""  # Initialize empty sequence
+    for (( i=0; i<$len; i++ )); do
+        # Get a random command from cmds
+        local cmd=${cmds[$RANDOM % ${#cmds[@]}]}
+        # Append command to the sequence
+        seq+="$cmd; "
+    done
+    # Print the generated sequence
+    echo "${seq%??}"  # remove the last "; "
+}
+
+# Check for user input
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <number_of_flows>"
+    exit 1
+fi
+
+# Number of flows to generate
+num_flows=$1
+
+# Sequence lengths to consider
+lengths=(10 15 20 25)
+
+# Output file
+output_file="res.txt"
+
+# Make sure the output file is empty
+> "$output_file"
+
+# Generate the specified number of flows
+for (( n=0; n<$num_flows; n++ )); do
+    # Randomly select a length
+    len=${lengths[$RANDOM % ${#lengths[@]}]}
+    # Generate a sequence of the selected length
+    seq=$(generate_sequence $len)
+    # Write the sequence to the output file
+    echo "$seq" >> "$output_file"
+done
+
+echo "Generated $num_flows flows into $output_file"
