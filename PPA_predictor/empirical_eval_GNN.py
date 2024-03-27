b7fdd03034ff3f4ac023987f067efdbf5025bd2e
GaryHU <swe1609011@gmail.com>
2024-03-27 17:31:49 +0800

    Optimize model hyperparameters with Optuna
    - Import `optuna` for hyperparameter optimization
    - Implement hyperparameter tuning for models with Optuna
    - Compare and evaluate the best models after hyperparameter optimization

--- PPA_predictor/empirical_eval_GNN.py
+++ PPA_predictor/empirical_eval_GNN.py
@@ -6,6 +6,7 @@ import dgl.function as fn
 from dgl.dataloading import GraphDataLoader
 from sklearn.model_selection import train_test_split
 from models4GNN import GCNModel, GATModel, GraphSAGEModel, APPNPModel, GINModel
+import optuna
 
 # Load the dataset
 dataset = dgl.load_graphs('./graph_dataset.bin')[0]
@@ -17,20 +18,32 @@ train_graphs, test_graphs = train_test_split(dataset, test_size=0.2, random_stat
 train_dataloader = GraphDataLoader(train_graphs, batch_size=32, shuffle=True)
 test_dataloader = GraphDataLoader(test_graphs, batch_size=32)
 
-# Models, loss function, and optimizer
-models = {
-    'GCN': GCNModel(train_graphs[0].ndata['op'].shape[1], 16, 1),
-    'GAT': GATModel(train_graphs[0].ndata['op'].shape[1], 8, 1, num_heads=8),
-    'GraphSAGE': GraphSAGEModel(train_graphs[0].ndata['op'].shape[1], 16, 1),
-    'APPNP': APPNPModel(train_graphs[0].ndata['op'].shape[1], 16, 1, k=10, alpha=0.1),
-    'GIN': GINModel(train_graphs[0].ndata['op'].shape[1], 16, 1, num_layers=2)
-}
-loss_func = nn.MSELoss()
+# Define the objective function for Optuna
+def objective(trial, model_name):
+    # Define hyperparameters to tune for each model
+    if model_name == 'GCN':
+        hid_feats = trial.suggest_int('hid_feats', 8, 64)
+        model = GCNModel(train_graphs[0].ndata['op'].shape[1], hid_feats, 1)
+    elif model_name == 'GAT':
+        hid_feats = trial.suggest_int('hid_feats', 4, 32)
+        num_heads = trial.suggest_int('num_heads', 1, 8)
+        model = GATModel(train_graphs[0].ndata['op'].shape[1], hid_feats, 1, num_heads)
+    elif model_name == 'GraphSAGE':
+        hid_feats = trial.suggest_int('hid_feats', 8, 64)
+        model = GraphSAGEModel(train_graphs[0].ndata['op'].shape[1], hid_feats, 1)
+    elif model_name == 'APPNP':
+        hid_feats = trial.suggest_int('hid_feats', 8, 64)
+        k = trial.suggest_int('k', 1, 10)
+        alpha = trial.suggest_float('alpha', 0.1, 0.9)
+        model = APPNPModel(train_graphs[0].ndata['op'].shape[1], hid_feats, 1, k, alpha)
+    elif model_name == 'GIN':
+        hid_feats = trial.suggest_int('hid_feats', 8, 64)
+        num_layers = trial.suggest_int('num_layers', 1, 5)
+        model = GINModel(train_graphs[0].ndata['op'].shape[1], hid_feats, 1, num_layers)
 
-# Training and evaluation
-for model_name, model in models.items():
+    loss_func = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-    
+
     # Training loop
     for epoch in range(100):
         model.train()
@@ -40,9 +53,45 @@ for model_name, model in models.items():
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-    
+
     # Evaluation
     model.eval()
+    total_error = 0
+    for batched_graph in test_dataloader:
+        pred = model(batched_graph, batched_graph.ndata['op'].float())
+        total_error += loss_func(pred, batched_graph.ndata['labels'].float()).item()
+
+    mean_error = total_error / len(test_dataloader)
+    return mean_error
+
+# Define the loss function
+loss_func = nn.MSELoss()
+
+# Models and hyperparameter tuning
+models = ['GCN', 'GAT', 'GraphSAGE', 'APPNP', 'GIN']
+
+best_models = {}
+for model_name in models:
+    study = optuna.create_study(direction='minimize')
+    study.optimize(lambda trial: objective(trial, model_name), n_trials=100)
+    best_params = study.best_params
+    best_model = None
+
+    if model_name == 'GCN':
+        best_model = GCNModel(train_graphs[0].ndata['op'].shape[1], best_params['hid_feats'], 1)
+    elif model_name == 'GAT':
+        best_model = GATModel(train_graphs[0].ndata['op'].shape[1], best_params['hid_feats'], 1, best_params['num_heads'])
+    elif model_name == 'GraphSAGE':
+        best_model = GraphSAGEModel(train_graphs[0].ndata['op'].shape[1], best_params['hid_feats'], 1)
+    elif model_name == 'APPNP':
+        best_model = APPNPModel(train_graphs[0].ndata['op'].shape[1], best_params['hid_feats'], 1, best_params['k'], best_params['alpha'])
+    elif model_name == 'GIN':
+        best_model = GINModel(train_graphs[0].ndata['op'].shape[1], best_params['hid_feats'], 1, best_params['num_layers'])
+
+    best_models[model_name] = best_model
+
+# Compare the best models
+for model_name, model in best_models.items():
     total_error = 0
     for batched_graph in test_dataloader:
         pred = model(batched_graph, batched_graph.ndata['op'].float())
