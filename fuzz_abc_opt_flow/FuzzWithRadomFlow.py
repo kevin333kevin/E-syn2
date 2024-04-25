df3deb0a29e09b6f89f80567bfa76d223af44c76
GaryHU <swe1609011@gmail.com>
2024-04-25 19:28:37 +0800

    update fuzzing script

--- fuzz_abc_opt_flow/FuzzWithRadomFlow.py
+++ fuzz_abc_opt_flow/FuzzWithRadomFlow.py
@@ -4,16 +4,33 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 # Function to execute a single ABC flow
 def execute_flow(flow):
     # Prepare the command with the given flow
-    command = "../abc/abc -c \"abcrc; read_eqn ../abc/opt.eqn; strash; {} ;read_lib ../abc/asap7_clean.lib ; map ; topo; upsize; dnsize; stime -d\"".format(flow)
+    command = "../abc/abc -c \"source -s ../abc/abc.rc; read_eqn ../abc/opt.eqn; strash; {} ;read_lib ../abc/asap7_clean.lib ; map ; topo; upsize; dnsize; stime -d\"".format(flow)
     # Execute the command
     os.system(command)
     
     return "Completed: {}".format(flow)
 
+# function to sampling flows from a given file
+def sampling_flows(list_flows, num_samples):
+    import random
+    random.shuffle(list_flows)
+    return list_flows[:num_samples]
+
+# randomly remove a operator from a flow
+def remove_operator(flow):
+    import random
+    # could be "rw" "rwz" "rf" "rfz" "balance" "resub" "resub -z" "resyn2" "compress2" "dch ; strash" "compress2rs"
+    operators = ["rw;", "rwz;", "rf;", "rfz;", "balance;", "resub;", "resub -z;", "resyn2;", "compress2;", "dch ; strash;", "compress2rs;"]
+    operator = random.choice(operators)
+    return flow.replace(operator, "")
+
 def main():
     # Load the flows from file
     with open('GeneratedFlows.txt', 'r') as file:
         flows = [line.strip() for line in file]
+        
+    flows = sampling_flows(flows, 50)  # Sample 1000 flows randomly
+    #flows = [remove_operator(flow) for flow in flows]  # Randomly remove an operator from each flow
     
     # Number of parallel processes (up to the number of cores/cpus you have)
     max_workers = 64
